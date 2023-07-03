517813494f9913326d0e9b4716db1c75fc1a5687
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-07-03 22:50:49 +0900

    db 연결 및 db 조회 테스트 코드 추가.

--- management_server/index.ts
+++ management_server/index.ts
@@ -1,10 +1,9 @@
 import express, {Express, Request, Response} from "express";
-import bodyParser from 'body-parser';
 import mysql from 'mysql';
 
 const port = 8000;
 
-//db 연결
+//db connector
 const connection : mysql.Connection = mysql.createConnection({
     host : 'ls-9c7d7b612085a406360965e6158e47d7564a40d7.c8heglnxvydw.ap-northeast-2.rds.amazonaws.com',
     port : 3306,
@@ -15,10 +14,10 @@ const connection : mysql.Connection = mysql.createConnection({
 
 connection.connect ((error)=>{
     if (error){
-        console.error('연결실패',error);
+        console.error('fail to connect',error);
         return;
     }
-    console.log('연결성공')
+    console.log('db connect success')
 });
 
 
