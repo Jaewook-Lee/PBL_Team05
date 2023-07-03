664744e1659d9e074451619b8ffbb0bd349c1e85
Jaewook-Lee <swljw@kookmin.ac.kr>
2023-07-03 14:48:01 +0900

    AD server를 위한 dummy data 작성, resposne 전달을 json으로 수정.

--- management_server/index.ts
+++ management_server/index.ts
@@ -13,26 +13,26 @@ app.get('/',(req : Request, res : Response)=>{
     res.sendFile(photoPath, {root : './'});
 });
 
-
-app.get('/AD/requestList',(req : Request, res : Response)=>{
-    res.json({"stauts": "success",
-            "data" : [{"content": "pic1.jpeg","width" : "dddddd"}]
-    });
-});
-
-
 app.get('/AD/readAd',(req : Request, res : Response)=>{
-    res.send({"stauts": "success",
-    "data" : [{"content": "pic1.jpeg","width" : "ssssss"}]
-})
+    res.json({
+        "adName": "대충 광고 이름",
+        "period": "2017-01-01 ~ 2017-01-31",
+        "unitPrice": "1000",
+        "advertiser": "대충 광고주",
+        "language": "ko",
+        "adType": "banner"
+    })
 });
 
 app.delete('/AD/deleteAd',(req : Request, res : Response)=>{
-    res.send({"stauts": "success", "message" : ""});
+    res.json({
+        "stauts": "success",
+        "message" : ""}
+    );
 });
 
 app.put('/AD/createAd',(req : Request, res : Response)=>{
-    res.send({
+    res.json({
         "status": "success",
         "message": "등록에 성공했습니다.",
         "adId": "0000"
@@ -40,14 +40,14 @@ app.put('/AD/createAd',(req : Request, res : Response)=>{
 });
 
 app.post('/AD/activeAd',(req : Request, res : Response)=>{
-    res.send({
+    res.json({
         "status": "success",
         "message": "active Success!"
     });
 });
 
 app.post('/AD/createAd',(req : Request, res : Response)=>{
-    res.send({
+    res.json({
         "status": "success",
         "message": "등록에 성공했습니다",
         "adId": "0000"
