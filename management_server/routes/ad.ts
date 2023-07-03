794ec9864480955a74538b0557f0c9e6e87161d7
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-07-04 00:46:10 +0900

    라우터 Path 수정

--- management_server/routes/ad.ts
+++ management_server/routes/ad.ts
@@ -4,24 +4,24 @@ import { Router,Request,Response } from "express";
 const router : Router = Router();
 
 // 이건 user가 list를 달라고 하는건데 더미코드가 필요해 여기 있는것
-router.get('/AD/requestList',(req : Request, res : Response)=>{
+router.get('/requestList',(req : Request, res : Response)=>{
     res.json({"stauts": "success",
             "data" : [{"content": "pic1.jpeg","width" : "dddddd"}]
     });
 });
 
 
-router.get('/AD/readAd',(req : Request, res : Response)=>{
+router.get('/readAd',(req : Request, res : Response)=>{
     res.send({"stauts": "success",
     "data" : [{"content": "pic1.jpeg","width" : "ssssss"}]
 })
 });
 
-router.delete('/AD/deleteAd',(req : Request, res : Response)=>{
+router.delete('/deleteAd',(req : Request, res : Response)=>{
     res.json({"stauts": "success", "message" : ""});
 });
 
-router.put('/AD/createAd',(req : Request, res : Response)=>{
+router.put('/createAd',(req : Request, res : Response)=>{
     res.json({
         "status": "success",
         "message": "등록에 성공했습니다.",
@@ -29,14 +29,14 @@ router.put('/AD/createAd',(req : Request, res : Response)=>{
     });
 });
 
-router.post('/AD/activeAd',(req : Request, res : Response)=>{
+router.post('/activeAd',(req : Request, res : Response)=>{
     res.json({
         "status": "success",
         "message": "active Success!"
     });
 });
 
-router.post('/AD/updateAd',(req : Request, res : Response)=>{
+router.post('/updateAd',(req : Request, res : Response)=>{
     res.send({
         "status": "success",
         "message": "업데이트에 성공했습니다",
@@ -45,7 +45,7 @@ router.post('/AD/updateAd',(req : Request, res : Response)=>{
 });
 
 //광고의 contents를 upload
-router.post('/AD/uploadContens',(req : Request, res : Response)=>{
+router.post('/uploadContens',(req : Request, res : Response)=>{
     res.send({
         "status": "success",
         "message": "업로드에 성공했습니다."
