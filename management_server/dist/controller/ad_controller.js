bb90d43a0f63760aef5ff4325f637d222aaa2dd6
Jaewook-Lee <swljw@kookmin.ac.kr>
2023-07-05 16:56:42 +0900

    Row가 0개일 경우에 에러인 경우와 동일하게 실패 메시지 전송, 기간을 API 문서에 맞게 수정

--- management_server/dist/controller/ad_controller.js
+++ management_server/dist/controller/ad_controller.js
@@ -41,36 +41,45 @@ exports.requestList = requestList;
 // db및 api 수정필요.
 //get
 function readAd(req, res) {
-    connection.query(`select ads.name, ads.period_begin, ads.period_end, ads.advertizer, ads.country, ad_contents_data.type, ad_statics.hit_count, ad_statics.hit_time_sum from ads inner join ad_contents_data on ads.id = ad_contents_data.id inner join ad_statics on ads.id = ad_statics.id where ads.id = ${req.query.adId}`, function (err, result) {
-        if (err) {
-            console.log(err);
-            // Todo : 빈 객체일 경우 실패 메시지 전달하도록 코드 작성해야될듯
-            res.json({
-                status: "fail",
-                message: "조회에 실패했습니다."
-            });
-        }
-        else {
-            const startDay = result[0].period_begin;
-            const startYear = startDay.getFullYear();
-            const startMonth = startDay.getMonth();
-            const startDate = startDay.getDate();
-            const endDay = result[0].period_end;
-            const endYear = endDay.getFullYear();
-            const endMonth = endDay.getMonth();
-            const endDate = endDay.getDate();
-            const period = `${startYear}-${startMonth}-${startDate} ~ ${endYear}-${endMonth}-${endDate}`;
-            res.json({
-                "adName": result[0].name,
-                "period": period,
-                "advertiser": result[0].advertizer,
-                "language": result[0].country,
-                "adType": result[0].type,
-                "hitCount": result[0].hit_count,
-                "watchTimeSum": result[0].hit_count_sum
-            });
-        }
-    });
+    if (req.query.adId === undefined) {
+        res.json({
+            status: "fail",
+            message: "조회에 실패했습니다."
+        });
+    }
+    else {
+        connection.query(`select ads.name, ads.period_begin, ads.period_end, ads.advertizer, ads.country, ad_contents_data.type, ad_statics.hit_count, ad_statics.hit_time_sum from ads inner join ad_contents_data on ads.id = ad_contents_data.id inner join ad_statics on ads.id = ad_statics.id where ads.id = ${req.query.adId}`, function (err, result) {
+            // DB 조회 과정에서 에러가 나거나, row 수가 0일 경우(JOIN 결과 row가 0개일 수 있다.) 실패 메시지 전달
+            if (err || result.length === 0) {
+                console.log(err);
+                console.log(`Result rows: ${result}`);
+                res.json({
+                    status: "fail",
+                    message: "조회에 실패했습니다."
+                });
+            }
+            else {
+                const startDay = result[0].period_begin;
+                const startYear = startDay.getFullYear();
+                const startMonth = startDay.getMonth();
+                const startDate = startDay.getDate();
+                const endDay = result[0].period_end;
+                const endYear = endDay.getFullYear();
+                const endMonth = endDay.getMonth();
+                const endDate = endDay.getDate();
+                const period = `${startYear}-${startMonth}-${startDate} ~ ${endYear}-${endMonth}-${endDate}`;
+                res.json({
+                    "adName": result[0].name,
+                    "period": period,
+                    "advertiser": result[0].advertizer,
+                    "language": result[0].country,
+                    "adType": result[0].type,
+                    "hitCount": result[0].hit_count,
+                    "watchTimeSum": result[0].hit_count_sum
+                });
+            }
+        });
+    }
 }
 exports.readAd = readAd;
 //delete
