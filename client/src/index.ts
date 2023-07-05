229b85cac6b6b3d892e0885e6b945cf383eeb305
hong-seonjae <ghdtjswo1915@kookmin.ac.kr>
2023-07-05 03:22:19 -0400

    4:22 seonjae

--- client/src/index.ts
+++ client/src/index.ts
@@ -24,3 +24,21 @@ const addPopupCloseButton = document.getElementById("ADD_cancel_button") as HTML
 addPopupCloseButton.onclick = () => {
     addPopup.style.display = 'none';
 }
+
+//edit 눌렀을때
+const editButton = document.getElementsByClassName("edit") as HTMLCollectionOf<HTMLButtonElement>;
+const editPopup = document.getElementsByClassName("when_click_edit") as HTMLCollectionOf<HTMLButtonElement>;
+
+for (let i = 0; i < editButton.length; i++) {
+    editButton[i].onclick = () => {
+        editPopup[i].style.display = 'flex';
+    }
+}
+const editPopupCloseButton = document.getElementsByClassName("list_edit_cancel_button") as HTMLCollectionOf<HTMLButtonElement>;
+
+
+for (let i = 0; i < editPopupCloseButton.length; i++) {
+    editPopupCloseButton[i].onclick = () => {
+        editPopup[i].style.display = 'none';
+    }
+}
