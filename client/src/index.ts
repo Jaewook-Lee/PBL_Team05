190dd0c435ca6d4b71ccbcff7a1a49eb58394f6f
hong-seonjae <ghdtjswo1915@kookmin.ac.kr>
2023-07-04 22:19:08 -0400

    0

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,15 @@
 import css from "./index.css";
 css;
 
-console.log("Hello World");
+const adTestPopup = document.getElementById("when_click_adtest") as HTMLDivElement;
+
+const adTestButton = document.getElementById("ADTest") as HTMLButtonElement;
+adTestButton.onclick = () => {
+    adTestPopup.style.display = "flex";
+};
+
+const adTestPopupCloseButton = document.getElementById("ok") as HTMLButtonElement;
+adTestPopupCloseButton.onclick = () => {
+    adTestPopup.style.display ="none";
+};
+
