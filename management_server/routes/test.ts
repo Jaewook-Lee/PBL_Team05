006edb7d0860fcf16420ba9a276a086d7923ba10
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-07-06 00:02:26 +0900

    forever 돌아가게 파일 수정

--- management_server/routes/test.ts
+++ management_server/routes/test.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
-import {requestList} from "../controller/test_controller";
+import {requestList,readAd,deleteAd,createAd,activeAd,requestAdminList} from "../controller/test_controller";
 const router : Router = Router();
 
 router.get('/requestList', requestList);  
+
+router.get('/readAd', readAd);
+
+router.delete('/deleteAd', deleteAd);
+
+router.post('/createAd', createAd);
+
+router.post('/activeAd', activeAd);
+
+// router.put('/updateAd', updateAd);
+
+// router.post('/uploadContents', uploadContents);    //광고의 contents를 upload
+
+router.get("/requestAdminList", requestAdminList);
+
 
 export default router;
