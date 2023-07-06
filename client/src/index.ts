a4b01c1f24f3c13d7c7fc7e17e8e030731cf7463
noname <48761044+noname0310@users.noreply.github.com>
2023-07-06 14:45:54 +0900

    sans

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,11 +1,14 @@
 import { AdListView } from "./AdListView";
+import { DeletePanelView } from "./DeletePanelVIew";
 import { DetailPanelView } from "./DetailPanelView";
+import { EditPanelView } from "./EditPanelView";
 import css from "./index.css";
 css;
 
 const detailPanelView = new DetailPanelView();
-
-const adListView = new AdListView(detailPanelView);
+const editPanelView = new EditPanelView();
+const deletePanelView = new DeletePanelView();
+const adListView = new AdListView(detailPanelView, editPanelView, deletePanelView);
 adListView.initialize();
 
 // ADDTest 눌렀을때
@@ -20,40 +23,16 @@ const adTestPopupCloseButton = document.getElementById("ADDTEST_ok_button") as H
 adTestPopupCloseButton.onclick = () => {
     adTestPopup.style.display = "none";
 };
+
 // ADD 눌렀을때
 const addButton = document.getElementById("ADD") as HTMLButtonElement;
 const addPopup = document.getElementById("when_click_ADD") as HTMLDivElement;
 
 addButton.onclick = () => {
-    addPopup.style.display = 'flex';
+    addPopup.style.display = "flex";
 };
 
 const addPopupCloseButton = document.getElementById("ADD_cancel_button") as HTMLButtonElement;
 addPopupCloseButton.onclick = () => {
-    addPopup.style.display = 'none';
+    addPopup.style.display = "none";
 };
-//상세 눌렀을때
-const deailPanel = document.getElementById("when_click_detail") as HTMLDivElement;
-
-const detailPopupCloseButton = document.getElementById("list_detail_cancel_button") as HTMLDivElement;
-detailPopupCloseButton.onclick = () => {
-    deailPanel.style.display = 'none'
-}
-
-
-//수정 눌렀을때
-const editPanel = document.getElementById("when_click_edit") as HTMLDivElement;
-
-const editPopupCloseButton = document.getElementById("list_edit_cancel_button") as HTMLButtonElement;
-editPopupCloseButton.onclick = () => {
-    editPanel.style.display = 'none';
-};
-
-//삭제 눌렀을때 
-const deletePanel = document.getElementById("when_click_delete") as HTMLDivElement;
-
-const deletePopupCloseButton = document.getElementById("list_delete_cancel_button") as HTMLButtonElement;
-deletePopupCloseButton.onclick = () => {
-    deletePanel.style.display = 'none';
-};
-
